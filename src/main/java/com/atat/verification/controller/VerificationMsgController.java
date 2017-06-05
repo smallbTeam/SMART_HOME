831e0d5ca0ef7a51036f40b7400c41de48454909
rainsunset <email@example.com>
2017-06-06 03:45:40 +0800

    addjsp

--- src/main/java/com/atat/verification/controller/VerificationMsgController.java
+++ src/main/java/com/atat/verification/controller/VerificationMsgController.java
@@ -4,8 +4,16 @@
  */
 package com.atat.verification.controller;
 
+import com.atat.common.base.controller.BaseController;
+import com.atat.common.util.StringUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 短信验证码
@@ -14,11 +22,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping(value = "verificationMsg")
-public class VerificationMsgController {
+public class VerificationMsgController extends BaseController{
 
     @RequestMapping(params = "action=sendMsg")
-    public void sendVerificationMsg(){
-
+    public void sendVerificationMsg(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String,Object> resultMap = new HashMap<String,Object>();
+        String strMobelPhone = request.getParameter("mobelPhone");
+        String password = request.getParameter("password");
+        if((StringUtil.isNotEmpty(strMobelPhone)) && (StringUtil.isNotEmpty(password))){
+            resultMap.put("result", "success");
+            resultMap.put("operationResult", strMobelPhone);
+        } else {
+            resultMap.put("result", "error");
+            resultMap.put("error", "手机号或密码为空");
+        }
+        this.renderJson(response,resultMap);
         //生成随机短信验证码
         //发送短信
         //成功-验证码存入session
